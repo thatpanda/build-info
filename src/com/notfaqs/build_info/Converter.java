468f029bf2e7568c3426a488336b72117caa9c0d
thatpanda <thatpanda@users.noreply.github.com>
2013-10-05 19:06:44 -0700

    Show display metrics

---
+++ src/com/notfaqs/build_info/Converter.java
@@ -0,0 +1,82 @@
+﻿package com.notfaqs.build_info;
+
+import android.content.res.Configuration;
+import android.util.DisplayMetrics;
+import android.view.Surface;
+
+public class Converter
+{
+    public static String densityDpiToString(int densityDpi)
+    {
+        switch(densityDpi)
+        {
+        case DisplayMetrics.DENSITY_LOW:
+            return "ldpi";
+        case DisplayMetrics.DENSITY_MEDIUM:
+            // Same as DENSITY_DEFAULT
+            return "mdpi";
+        case DisplayMetrics.DENSITY_TV:
+            return "tvdpi";
+        case DisplayMetrics.DENSITY_HIGH:
+            return "hdpi";
+        case DisplayMetrics.DENSITY_XHIGH:
+            return "xhdpi";
+        case DisplayMetrics.DENSITY_XXHIGH:
+            return "xxhdpi";
+        case DisplayMetrics.DENSITY_XXXHIGH:
+            return "xxxhdpi";
+        }
+        return "unknown";
+    }
+    
+    @SuppressWarnings("deprecation")
+    public static String orientationToString(int orientation)
+    {
+        switch(orientation)
+        {
+        case Configuration.ORIENTATION_UNDEFINED:
+            return "undefined";
+        case Configuration.ORIENTATION_PORTRAIT:
+            return "portrait";
+        case Configuration.ORIENTATION_LANDSCAPE:
+            return "landscape";
+        case Configuration.ORIENTATION_SQUARE:
+            return "square";
+        }
+        return "unknown";
+    }
+    
+    public static String rotationToString(int rotation)
+    {
+        switch(rotation)
+        {
+        case Surface.ROTATION_0:
+            return "0°";
+        case Surface.ROTATION_90:
+            return "90°";
+        case Surface.ROTATION_180:
+            return "180°";
+        case Surface.ROTATION_270:
+            return "270°";
+        }
+        return "unknown";
+    }
+    
+    public static String screenSizeToString(int screenSize)
+    {
+        switch(screenSize)
+        {
+        case Configuration.SCREENLAYOUT_SIZE_UNDEFINED:
+            return "undefined";
+        case Configuration.SCREENLAYOUT_SIZE_SMALL:
+            return "small";
+        case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+            return "normal";
+        case Configuration.SCREENLAYOUT_SIZE_LARGE:
+            return "large";
+        case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+            return "xlarge";
+        }
+        return "unknown";
+    }
+}
--- src/com/notfaqs/build_info/MainActivity.java
+++ src/com/notfaqs/build_info/MainActivity.java
@@ -7,9 +7,12 @@ package com.notfaqs.build_info;
 import java.lang.reflect.Field;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
@@ -22,6 +25,8 @@ public class MainActivity extends Activity
         setTitle(R.string.dialog_title);
         
         final StringBuilder sb = new StringBuilder();
+        sb.append(getString(R.string.display_header) + "\n");
+        sb.append(getDisplayMetrics() + "\n");
         sb.append(getString(R.string.build_header) + "\n");
         sb.append(getFields(new android.os.Build()) + "\n");
         sb.append(getString(R.string.build_version_header) + "\n");
@@ -30,6 +35,64 @@ public class MainActivity extends Activity
         final TextView t = (TextView) findViewById(R.id.label);
         t.setMovementMethod(new ScrollingMovementMethod());
         t.setText(sb.toString());
+    }
+    
+    private String getDensity()
+    {
+        final Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        display.getMetrics(dm);
+        return String.format(getString(R.string.density_format),
+                             dm.density,
+                             Converter.densityDpiToString(dm.densityDpi),
+                             dm.densityDpi);
+    }
+    
+    private String getOrientation()
+    {
+        final Configuration config = getResources().getConfiguration();
+        return String.format(getString(R.string.orientation_format),
+                             Converter.orientationToString(config.orientation));
+    }
+    
+    private String getResolution()
+    {
+        final Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        display.getMetrics(dm);
+        return String.format(getString(R.string.resolution_format),
+                             Math.max(dm.widthPixels, dm.heightPixels),
+                             Math.min(dm.widthPixels, dm.heightPixels));
+    }
+    
+    private String getRotation()
+    {
+        final Display display = getWindowManager().getDefaultDisplay();
+        return String.format(getString(R.string.rotation_format),
+                             display.getRotation(),
+                             Converter.rotationToString(display.getRotation()));
+    }
+    
+    private String getScreenSize()
+    {
+        final int screenLayout = getResources().getConfiguration().screenLayout;
+        final int screenSize = screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        return String.format(getString(R.string.screen_size_format),
+                             Converter.screenSizeToString(screenSize));
+    }
+    
+    private String getDisplayMetrics()
+    {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(getDensity() + "\n");
+        sb.append(getOrientation() + "\n");
+        sb.append(getResolution() + "\n");
+        sb.append(getRotation() + "\n");
+        sb.append(getScreenSize() + "\n");
+        
+        final String result = sb.toString();
+        Log.i("Display metrics", result);
+        return result;
     }
     
     private String getFields(final Object obj)
